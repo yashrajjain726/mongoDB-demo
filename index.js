83c0321a3b84e2dd757acfb569519d9917b56da1
Yashraj Jain <yashrajjain726@gmail.com>
2021-05-26 18:58:47 +0530

    schema , model and filtering data done

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-const mongoose = 
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/playground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.log("Couldnot connect to database", err));
+
+// Defining schema for database
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+// Creating a model
+
+// Step 1 -> making a class of the schema
+const Course = mongoose.model("Course", courseSchema);
+async function createCourse() {
+  // Step 2 -> Making object of the class .
+  const course = new Course({
+    name: "React.js Course",
+    author: "Yashraj Jain",
+    tags: ["react", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function getCourses() {
+  const courses = await Course.find({
+    author: "Yashraj Jain",
+    isPublished: "true",
+  })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({name:1,tags:1})
+  console.log(courses);
+}
+getCourses();
